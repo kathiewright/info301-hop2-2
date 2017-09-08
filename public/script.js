b6c3c199a1d50257a3420bec0e0f95b7fb531ed2
System <none>
2017-09-08 17:23:18 +0000

    🌗🙅 Updated with Glitch

--- public/script.js
+++ public/script.js
@@ -3,12 +3,3 @@
       Date:
       Purpose:
 */ 
-
-function submitInfo(){
-  var name = document.getElementById("nameinput");
-  var email = document.getElementById("emailinput");
-  var phone = document.getElementById("phoneinput");
-  (name.value && email.value && phone.value) ? alert ("Thank you!") : alert ("Please fill in all fields.");
-}
-
-document.getElementById("submit").addEventListener("click", submitInfo, false);
